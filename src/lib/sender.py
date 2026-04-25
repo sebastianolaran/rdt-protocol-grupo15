aad1650912d2fc1fc7fe8e861a2fec0206349517
sebastian <solaran@fi.uba.ar>
2026-04-25 15:05:06 -0300

    interfaz de serder y receiver

---
+++ src/lib/sender.py
@@ -0,0 +1,40 @@
+import socket
+from abc import ABC, abstractmethod
+
+from lib.protocol import CONSTANTS
+
+
+class Sender(ABC):
+
+    def __init__(self, sock: socket.socket, addr: tuple, verbose: bool = False):
+        self.sock    = sock
+        self.addr    = addr
+        self.verbose = verbose
+        self.next_np = 1
+        self.last_np = 0
+        self.max_retries = CONSTANTS["MAX_RETRIES"]
+
+    def _send_raw(self, pkt: bytes) -> None:
+        self.sock.sendto(pkt, self.addr)
+
+    def _recv_ack(self) -> bytes:
+        if self.recv_queue:
+            return self.recv_queue.get(timeout=CONSTANTS["DEFAULT_TIMEOUT"])
+        else:
+            self.sock.settimeout(CONSTANTS["DEFAULT_TIMEOUT"])
+            raw, _ = self.sock.recvfrom(CONSTANTS["MAX_PKT_SIZE"])
+            return raw
+
+    @abstractmethod
+    def send_file(self, filepath: str) -> None:
+        pass
+
+    @abstractmethod
+    def send_bytes(self, data: bytes) -> None:
+        pass
+
+    @abstractmethod
+    def close(self) -> None:
+        pass
+
+    
